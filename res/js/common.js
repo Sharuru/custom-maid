55f5f4ebfcd6a6de8fa856ee48ef6f0e723b0db1
Sharuru <mave@foxmail.com>
2015-11-08 13:07:18 +0000

    首页天气信息实现
    基本的 API 调用思路

--- res/js/common.js
+++ res/js/common.js
@@ -1,29 +1,38 @@
+//进入 APP 时初始化操作
 function appInitialize() {
 	var localStorage = window.localStorage;
 	if (localStorage.getItem("isFirstRun") == "0") {
-		console.log("Welcome back");
+		console.log('Welcome back');
 	} else {
-		console.log("First run, creating file");
-		localStorage.setItem("isFirstRun", "0");
+		console.log('First run, creating file');
+		localStorage.setItem('isFirstRun', '0');
 		//设置 API key
-		localStorage.setItem("weatherAPIKey", "96d701108326a3e0447fb0008cf526a9");
+		//天气 API key
+		localStorage.setItem('weatherAPIKey', 'ffeb476b3fe24929959cfadd168fdf1d');
 	}
 }
 
-function getJson(reqUrl) {
-	mui.ajax('https://api.heweather.com/x3/weather?city=shanghai&key=ffeb476b3fe24929959cfadd168fdf1d', {
-		dataType: 'json', //服务器返回json格式数据
-		type: 'get', //HTTP请求类型
-		success: function(data) {
-			console.log("String --> " + JSON.stringify(data));
-			console.log(data['HeWeather data service 3.0'][0].aqi.city.aqi);
-//			mui.each(data, function(index, item) {
-//				console.log(item[0].aqi);
-//			})
+//根据 index 获取对应 API
+function getAPIKey(index) {
+	return window.localStorage.getItem(index);
+}
+
+function getJsonObj(reqUrl) {
+	mui.ajax(reqUrl,{
+		dataType:'json',//服务器返回json格式数据
+		type:'post',//HTTP请求类型
+		async: false,
+		timeout:10000,//超时时间设置为10秒；
+		success:function(data){
+			console.log("Get json: " + JSON.stringify(data));
+			returnData = data;
 		},
-		error: function(data) {
-			console.log('err');
+		error:function(xhr,type,errorThrown){
+			//异常处理；
+			console.log('Err: ' + type);
 		}
 	});
-	console.log("CPL");
+	return returnData;
 }
+
+
--- res/js/weather.js
+++ res/js/weather.js
@@ -1,4 +1,54 @@
-// 设置首页天气信息
-function setIndexWeatherInfo(){
-	getJson("http://apis.baidu.com/heweather/weather/free?city=beijing");
+//设置首页天气信息
+function setIndexWeatherInfo() {
+	console.log('In setIndexWeatherInfo');
+	var localStorage = window.localStorage;
+	if (localStorage.getItem('cachedWeatherInfo') != null) {
+		//本地存在缓存天气记录
+		console.log('Weather info have cache.');
+		//TODO 比对 UTC 时间进行更新检测
+	} else {
+		console.log('Weather info need update');
+		//需要更新天气信息的场合
+		//城市硬编码
+		var respObj = getWeatherInfo('shanghai');
+		console.log('Get weather json: ' + JSON.stringify(respObj));
+		//获得指定数据
+		var nowTmp = respObj['HeWeather data service 3.0'][0].now.tmp;
+		var todayMinTmp = respObj['HeWeather data service 3.0'][0].daily_forecast[0].tmp.min;
+		var todayMaxTmp = respObj['HeWeather data service 3.0'][0].daily_forecast[0].tmp.max;
+		var nowCond = respObj['HeWeather data service 3.0'][0].now.cond.txt;
+		var nowWind = respObj['HeWeather data service 3.0'][0].now.wind.dir;
+		var todayPm25 = respObj['HeWeather data service 3.0'][0].aqi.city.pm25;
+		var todayQlty = respObj['HeWeather data service 3.0'][0].aqi.city.qlty;
+		//设置画面
+		var d = new Date();
+		document.getElementById('nowTmp').innerText = nowTmp + '℃';
+		document.getElementById('todayTmp').innerText = todayMinTmp + '℃/' + todayMaxTmp + '℃';
+		document.getElementById('nowCond').innerText = nowCond;
+		document.getElementById('today').innerText = getWeekday(d.getDay()) + ' ' +  d.getMonth() + '/' + d.getDate();
+		document.getElementById('nowWind').innerText = nowWind;
+		document.getElementById('todayQlty').innerText = todayQlty;
+		document.getElementById('todayPm25').innerText = todayPm25;
+	
+	}
+}
+
+//获取天气信息
+function getWeatherInfo(reqCity) {
+	//拼接请求地址
+	var reqUrl = 'https://api.heweather.com/x3/weather?city=' + reqCity + '&key=' + getAPIKey('weatherAPIKey');
+	return getJsonObj(reqUrl);
+}
+
+// 获得星期
+function getWeekday(day){
+	var weekDays = new Array(7);
+	weekDays[0] = "周日";
+	weekDays[1] = "周一";
+	weekDays[2] = "周二";
+	weekDays[3] = "周三";
+	weekDays[4] = "周四";
+	weekDays[5] = "周五";
+	weekDays[6] = "周六";
+	return weekDays[day];
 }
