29e281b24609125b05fb6e027e7ade3e1a3d0dc4
S1507109 <S1507109@8fe30038-93e8-d64f-a5c7-3f0df5b1f493>
2015-11-09 09:42:33 +0000

    生活 添加common.js 文件

---
+++ life/js/common.js
@@ -0,0 +1,45 @@
+//进入 APP 时初始化操作
+function appInitialize() {
+	var localStorage = window.localStorage;
+	if (localStorage.getItem("isFirstRun") == "0") {
+		//再次打开
+		console.log('Welcome back');
+		mui.toast('お帰りなさいご主人様');
+	} else {
+		//初次安装设置配置缓存
+		console.log('First run, creating file');
+		mui.toast('初めまして');
+		localStorage.setItem('isFirstRun', '0');
+		//设置 API key
+		//天气 API key
+		localStorage.setItem('weatherAPIKey', 'ffeb476b3fe24929959cfadd168fdf1d');
+		//百度 API key
+		localStorage.setIem('baiduAPIKey','Wikpdmp0xVzHMSDYjQ5arfAi');
+		
+	}
+}
+
+//根据 index 获取对应 API
+function getAPIKey(index) {
+	return window.localStorage.getItem(index);
+}
+
+function getJsonObj(reqUrl) {
+	mui.ajax(reqUrl,{
+		dataType:'json',//服务器返回json格式数据
+		type:'post',//HTTP请求类型
+		async: false,
+		timeout:5000,//超时时间设置为5秒；
+		success:function(data){
+			console.log("Get json: " + JSON.stringify(data));
+			returnData = data;
+		},
+		error:function(xhr,type,errorThrown){
+			//异常处理；
+			console.log('Err: ' + type);
+		}
+	});
+	return returnData;
+}
+
+
