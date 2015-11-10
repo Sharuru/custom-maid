234ef460666887c30e3ca1ede10c5d7bf5d2d721
Sharuru <mave@foxmail.com>
2015-11-10 09:24:01 +0000

    Bug 待确认

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ function setIndexWeatherInfo() {
 		if (localUTCDay < 10) {
 			localUTCDay = '0' + localUTCDay;
 		}
-		var localUTCHour = d.getUTCHours();
+		//TODO:BUG 确认
+		var localUTCHour = d.getUTCHours() - 1;
 		//补足两位数
 		if (localUTCHour < 10) {
 			localUTCHour = '0' + localUTCHour;
 		}
-		console.log(localUTCDay + ',' + localUTCHour + ',' + cacheUTCDay + ',' + cacheUTCHour + ',' + cachedUTC);
+		mui.alert(localUTCDay + ',' + localUTCHour + ',' + cacheUTCDay + ',' + cacheUTCHour + ',' + cachedUTC);
 		//比对更新时间
 		if (localUTCDay != cacheUTCDay || localUTCHour != cacheUTCHour) {
 			getIndexWeatherInfo();
