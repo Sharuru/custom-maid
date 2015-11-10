06fcdc14082a5653b3fefb4620cdecbe86aec24e
Sharuru <mave@foxmail.com>
2015-11-10 03:29:00 +0000

    首页 Cleanup

--- index.js
+++ index.js
@@ -88,3 +88,63 @@ function getLocation() {
 		provider: 'baidu'
 	});
 }
+
+function afterLoad() {
+	console.log("当前页面URL：" + plus.webview.currentWebview().getURL());
+	plus.webview.currentWebview().setStyle({
+		scrollIndicator: 'none'
+	});
+	//检测是否首次启动程序
+	appInitialize();
+	//设置首页天气信息
+	setIndexWeatherInfo();
+	//页面加载完毕后绑定事件
+	document.getElementById('btnGetLocation').addEventListener('tap', function() {
+		getLocation();
+	});
+	document.getElementById('navLifeDiv').addEventListener('tap', function() {
+		mui.openWindow({
+			url: 'life/lifeMain.html',
+			id: 'lifeMain',
+			extras: {
+				passNowCond: '今日天气：' + document.getElementById('nowCond').innerText,
+				passTodayTmp: '温度：' + document.getElementById('todayTmp').innerText,
+				passNowWind: '风向:' + document.getElementById('nowWind').innerText
+			}
+		});
+	});
+	document.getElementById('navTravelDiv').addEventListener('tap', function() {
+		mui.openWindow({
+			url: 'outing/outingMain.html',
+			id: 'outgingMain'
+		});
+	});
+	document.getElementById('navFinanceDiv').addEventListener('tap', function() {
+		mui.openWindow({
+			url: 'finance/financeMain.html',
+			id: 'financeMain'
+		});
+	});
+	//禁止滚动
+	window.addEventListener('tap', function() {
+		mui('#maidContent').scroll().setStopped(true); //暂时禁止滚动
+		var webview = plus.webview.currentWebview();
+	});
+	//首页返回键处理
+	//处理逻辑：1秒内，连续两次按返回键，则退出应用；
+	var first = null;
+	mui.back = function() {
+		//首次按键，提示‘再按一次退出应用’
+		if (!first) {
+			first = new Date().getTime();
+			mui.toast('再按一次退出应用');
+			setTimeout(function() {
+				first = null;
+			}, 1000);
+		} else {
+			if (new Date().getTime() - first < 1000) {
+				plus.runtime.quit();
+			}
+		}
+	};
+}
