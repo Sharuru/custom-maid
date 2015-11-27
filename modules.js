34cdae9a824befebf63385cdeef3fcb4a2a28d3a
Sharuru <mave@foxmail.com>
2015-11-27 04:38:56 +0000

    基本技术验证完成

--- modules.js
+++ modules.js
@@ -1,11 +1,25 @@
 //设置功能列表
 function setModuleList() {
-	console.log("Setting modules...");
+	console.log("Setting module list...");
 	//获取可用模块列表
 	var localStorage = window.localStorage;
 	var avalModuleList = localStorage.getItem("modules").split(",");
 	for (var i = 0; i < avalModuleList.length; i++) {
-		document.getElementById('moduleBody').innerHTML += '<div style="width:25%;height:80px;float:left;border:1px solid red;}">' + avalModuleList[i] + '</div>';
+		setModuleBlock(avalModuleList[i]);
 	}
 	console.log("Setting modules finished.");
+}
+
+//添加功能模块
+function setModuleBlock(moduleId) {
+console.log('Se');
+	var moduleSize = document.body.clientWidth / 4;
+	var contentStr = "";
+	contentStr +='<div style="width:' + moduleSize + 'px;height:' + moduleSize+'px;float:left;border-bottom:1px solid lightgray;border-right:1px solid lightgray;">';
+	contentStr += '		<div style="text-align: center;height:60%;padding-top: 20%;">';
+	contentStr += '			<img src="res/images/icons/modules/' + moduleId + '.png" width=45%></img>';
+	contentStr += '		</div>';
+	contentStr += '		<div style="text-align: center;height:40%;font-size: 12px;color:black;">'+moduleId +'</div>';
+	contentStr += '</div>'
+	document.getElementById('moduleBody').innerHTML += contentStr;
 }
