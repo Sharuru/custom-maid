36d4f859784c84bc985a8fada8621cb5d877d78c
Sharuru <mave@foxmail.com>
2015-12-24 09:35:30 +0000

    Cleanup

--- modules/HJ001/js/HJ001.js
+++ modules/HJ001/js/HJ001.js
@@ -3,8 +3,13 @@
  */
 function initializeHJ001() {
 	console.log('In HJ001');
-	//嵌入页面
-	var wv = plus.webview.create('http://typhoon.zjwater.gov.cn/wap.htm');
-	wv.setStyle({top:"49px",bottom:"0px;"});
-	wv.show();
+	//延迟200ms后嵌入显示页面
+	setTimeout(function() {
+		var wv = plus.webview.create('http://typhoon.zjwater.gov.cn/wap.htm');
+		wv.setStyle({
+			top: "49px",
+			bottom: "0px;"
+		});
+		wv.show();
+	}, 200);
 }
