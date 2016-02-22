98595275ff6b176b277c31a2af0b5734a54802a5
Sharuru <mave@foxmail.com>
2016-02-22 12:42:02 +0900

    Backup

---
+++ modules/HJ002/JS/HJ002.js
@@ -0,0 +1,16 @@
+/*
+ * HJ001 画面初始化
+ */
+function initializeHJ001() {
+	console.log('In HJ001');
+	//延迟200ms后嵌入显示页面
+	setTimeout(function() {
+		var wv = plus.webview.create('http://www.pm25.com/shanghai.html');
+		var wv = plus.webview.create('http://www.cnemc.cn/citystatus/waterWeekReportMore.jsp?selWaterSystem=%BC%AA%C1%D6%BC%AA%C1%D6%CF%AA%C0%CB%BF%DA&chkmWaterSystem');
+		wv.setStyle({
+			top: "49px",
+			bottom: "0px;"
+		});
+		wv.show();
+	}, 200);
+}
