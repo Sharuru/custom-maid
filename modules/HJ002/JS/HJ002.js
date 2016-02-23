c30b015047da3efcb2a99d79259a4713acf1849a
Sharuru <mave@foxmail.com>
2016-02-23 09:55:24 +0900

    Add HJ002 page

--- modules/HJ002/JS/HJ002.js
+++ modules/HJ002/JS/HJ002.js
@@ -6,7 +6,7 @@ function initializeHJ001() {
 	//延迟200ms后嵌入显示页面
 	setTimeout(function() {
 		var wv = plus.webview.create('http://www.pm25.com/shanghai.html');
-		var wv = plus.webview.create('http://www.cnemc.cn/citystatus/waterWeekReportMore.jsp?selWaterSystem=%BC%AA%C1%D6%BC%AA%C1%D6%CF%AA%C0%CB%BF%DA&chkmWaterSystem');
+		//var wv = plus.webview.create('http://www.cnemc.cn/citystatus/waterWeekReportMore.jsp?selWaterSystem=%BC%AA%C1%D6%BC%AA%C1%D6%CF%AA%C0%CB%BF%DA&chkmWaterSystem');
 		wv.setStyle({
 			top: "49px",
 			bottom: "0px;"
