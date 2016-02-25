b2467ef53892c1363170e93031d00c96f03f1001
Sharuru <mave@foxmail.com>
2016-02-25 10:09:22 +0900

    Add HJ002

--- modules/HJ002/JS/HJ002.js
+++ modules/HJ002/JS/HJ002.js
@@ -1,12 +1,11 @@
 /*
- * HJ001 画面初始化
+ * HJ002 画面初始化
  */
-function initializeHJ001() {
-	console.log('In HJ001');
+function initializeHJ002() {
+	console.log('In HJ002');
 	//延迟200ms后嵌入显示页面
 	setTimeout(function() {
-		var wv = plus.webview.create('http://www.pm25.com/shanghai.html');
-		//var wv = plus.webview.create('http://www.cnemc.cn/citystatus/waterWeekReportMore.jsp?selWaterSystem=%BC%AA%C1%D6%BC%AA%C1%D6%CF%AA%C0%CB%BF%DA&chkmWaterSystem');
+		var wv = plus.webview.create('http://zfb.ipe.org.cn/index.htm');
 		wv.setStyle({
 			top: "49px",
 			bottom: "0px;"
