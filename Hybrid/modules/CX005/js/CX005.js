39ab7fa13fb45888be4be0d9e8fdb13bb0dfb295
Sharuru <mave@foxmail.com>
2016-05-04 19:01:38 +0900

    Merge from upstream

--- Hybrid/modules/CX005/js/CX005.js
+++ Hybrid/modules/CX005/js/CX005.js
@@ -1,21 +1,48 @@
+var curPointLat = '';
+var curPointLng = '';
+
 function initializeCX005() {
 	mui.toast('正在载入中...');
-	var heightPar = document.body.clientHeight - 49;
-	mui('.mui-content')[0].innerHTML = '<div style="width: 100%;height: ' + heightPar + 'px;position: fixed" id="mapLBS"></div>';
-	var mapObj = new BMap.Map('mapLBS');
+	var heightPar = document.body.clientHeight - 49 - mui('.search-block')[0].offsetHeight;
+	document.getElementById('mapLBS').style.height = heightPar + 'px';
+	getLocation(initMapObj);
+}
+
+function getLocation(callback) {
 	var geolocationObj = new BMap.Geolocation();
 	geolocationObj.getCurrentPosition(function(r) {
 		if (this.getStatus() == BMAP_STATUS_SUCCESS) {
-			var markerObj = new BMap.Marker(r.point);
-			mapObj.addOverlay(markerObj);
-			mapObj.centerAndZoom(r.point, 16);
-			mapObj.addControl(new BMap.ScaleControl({
-				anchor: BMAP_ANCHOR_BOTTOM_LEFT
-			}))
+			curPointLat = r.point.lat;
+			curPointLng = r.point.lng;
+			callback();
 		} else {
 			mui.toast('Failed:' + this.getStatus());
 		}
 	}, {
 		enableHighAccuracy: true
 	});
+}
+
+function initMapObj() {
+	var mapObj = new BMap.Map('mapLBS');
+	var curPointObj = new BMap.Point(curPointLng, curPointLat);
+	var markerObj = new BMap.Marker(curPointObj);
+	mapObj.addOverlay(markerObj);
+	mapObj.centerAndZoom(curPointObj, 15);
+	var scaleOpts = {
+		anchor: BMAP_ANCHOR_BOTTOM_RIGHT,
+		offset: new BMap.Size(5, 5)
+	};
+	var navigationOpts = {
+		anchor: BMAP_ANCHOR_BOTTOM_LEFT,
+		type: BMAP_NAVIGATION_CONTROL_ZOOM,
+		offset: new BMap.Size(5, 30)
+	}
+	var geolocationOpts = {
+		anchor: BMAP_ANCHOR_BOTTOM_LEFT,
+		offset: new BMap.Size(5, 105)
+	}
+	mapObj.addControl(new BMap.ScaleControl(scaleOpts));
+	mapObj.addControl(new BMap.NavigationControl(navigationOpts));
+	mapObj.addControl(new BMap.GeolocationControl(geolocationOpts));
 }
